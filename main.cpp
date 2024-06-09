6b46f30401e4015bad1d6acc3abd66d39a98965b
Acin <acintya.sudarsono@gmail.com>
2024-06-09 20:51:23 +0700

    Declare class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+//Declare class Mahasiswa
+class Mahasiswa {
+	public:
+	  struct Data {
+		long long int NRP;
+		string nama;
+		int tugas[3];
+	};
+};
 
 int main() {
   cout << "init";
