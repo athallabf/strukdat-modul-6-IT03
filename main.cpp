4cb41f8ba7cbc543c006903470e7a7f844ed3517
AthallaITS <5027231018@student.its.ac.id>
2024-06-09 21:10:45 +0700

    add write function

--- main.cpp
+++ main.cpp
@@ -6,15 +6,31 @@ using namespace std;
 
 //Declare class Mahasiswa
 class Mahasiswa {
-	public:
-	  struct Data {
+public:
+	struct Data {
 		long long int NRP;
 		string nama;
 		int tugas[3];
+		};
+
+	void write(Data mhs) {
+		ofstream file("data_mahasiswa.txt", ios::app);
+		if (!file.is_open()) {
+			cout << "File tidak dapat diakses" << endl;
+			}
+		else {
+			file << mhs.NRP << " " << mhs.nama << " ";
+			for (int i = 0; i < 3; i++) {
+				file << mhs.tugas[i] << " ";
+				}
+			file << endl;
+			file.close();
+			cout << "Data berhasil dimasukkan!" << endl;
+			}
+		}
 	};
-};
 
 int main() {
-  cout << "init";
-  return 0;
-  }
+	cout << "init";
+	return 0;
+	}
