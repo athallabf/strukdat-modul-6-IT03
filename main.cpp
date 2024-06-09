8ad24a63c4cc2892fa1cc607602a0f39501e1770
Adis78 <adistariop@gmail.com>
2024-06-09 21:26:58 +0700

    add read function

--- main.cpp
+++ main.cpp
@@ -65,6 +65,38 @@ public:
 
         remove("data_mahasiswa.txt");
         rename("datatemp.txt", "data_mahasiswa.txt");
+        }// READ
+    void read() {
+        ifstream file("data_mahasiswa.txt");
+        if (file.is_open()) {
+            string line;
+            int i = 1;
+            while (getline(file, line)) {
+                istringstream iss(line);
+                Data mhs;
+                if (!(iss >> mhs.NRP)) {
+                    cerr << "Kesalahan parsing NRP untuk mahasiswa nomor " << i << endl;
+                    continue;
+                    }
+                iss >> ws; // Mengabaikan whitespace sebelum nama
+                getline(iss, mhs.nama, ' '); // Membaca nama hingga spasi berikutnya
+                for (int j = 0; j < 3; j++) {
+                    if (!(iss >> mhs.tugas[j])) {
+                        cerr << "Kesalahan parsing tugas " << j + 1 << " untuk mahasiswa nomor " << i << endl;
+                        continue;
+                        }
+                    }
+                cout << i << "\t" << mhs.NRP << "\t" << mhs.nama << "\t\t" << mhs.tugas[0] << "\t" << mhs.tugas[1] << "\t" << mhs.tugas[2] << endl;
+                i++;
+                }
+            if (i == 1) {
+                cout << "Data masih kosong!" << endl;
+                }
+            file.close();
+            }
+        else {
+            cerr << "File tidak dapat diakses!" << endl;
+            }
         }
 	};
 	
